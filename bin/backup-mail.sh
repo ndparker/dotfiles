d7850203c469f83f6d1ff9f5d963f27ee7475778
ndparker <ndparker@users.noreply.github.com>
2019-12-24 22:43:11 +0100

    kde update

--- bin/backup-mail.sh
+++ bin/backup-mail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-pimsettingexporterconsole --template ~/mail.backup/settings-template.xml --export ~/mail.backup/Settings-$(date +%Y-%m-%d).zip
+pimdataexporterconsole --template ~/mail.backup/settings-template.xml --export ~/mail.backup/Settings-$(date +%Y-%m-%d).zip
 
 running=
 while true; do
